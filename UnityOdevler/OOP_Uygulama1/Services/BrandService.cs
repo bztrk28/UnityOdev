a26dc8bb1fcbaf2ca689f294225a9ab7054f9dd9
İbrahim Birkan Öztürk <65361247+bztrk28@users.noreply.github.com>
2024-04-16 15:48:13 +0300

    Add files via upload

---
+++ UnityOdevler/OOP_Uygulama1/Services/BrandService.cs
@@ -0,0 +1,59 @@
+﻿using OOP_Uygulama1.Exceptions;
+using OOP_Uygulama1.Models;
+using OOP_Uygulama1.Repository;
+using System.Threading.Channels;
+using System.Collections.Generic;
+
+namespace OOP_Uygulama1.Services;
+
+public class BrandService
+{
+    
+    private BrandRepository _brandRepository;
+
+    public BrandService()
+    {
+        _brandRepository = new BrandRepository();
+    }
+
+    public void Add(Brand brand)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(brand.Name))
+            {
+                throw new BusinessException("Marka adı boş olamaz.");
+            }
+
+            _brandRepository.Add(brand);
+        }
+        catch (BusinessException ex)
+        {
+            Console.WriteLine("İş istisnası oluştu:");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Beklenmeyen bir hata oluştu:");
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public void GetAll()
+    {
+        List<Brand> brands = _brandRepository.GetAll();
+        brands.ForEach(x => Console.WriteLine(x));
+    }
+
+    public void Delete(int id)
+    {
+        _brandRepository.Delete(id);
+    }
+
+    public Brand GetById(int id)
+    {
+        return _brandRepository.GetById(id);
+    }
+    
+
+}
---
+++ UnityOdevler/OOP_Uygulama2/Repository/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Uygulama2.Models;
+
+namespace OOP_Uygulama2.Repository;
+
+public interface IBookRepository
+{
+    void Add(Book book);
+
+    List<Book> GetAll();
+
+    Book GetById(int id);
+
+    void Delete(int id);
+}
